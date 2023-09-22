73803a0b4562f9f75bed83a9290a242f656b9854
Zan-Kir <murilo4santos@hotmail.com>
2023-09-22 17:20:05 -0300

    Atualização receitas

--- Algoritmos/Aula5/ex04.py
+++ Algoritmos/Aula5/ex04.py
@@ -5,11 +5,11 @@ z = float(input("Digite um valor para z: "))
 if x + y > z and y + x > z and z + y > x:
     e_triangulo = f"Ele é um triângulo "
     if (x == y) or (x == z) or (y == z):
-        tipo = "Isóceles"
+        e_triangulo = f"Ele é um triângulo. Isóceles"
     elif (x == y) and (x == z):
-        tipo = "Equilátero"
+        e_triangulo = f"Ele é um triângulo. Equilátero"
     elif (x != y != z):
-        tipo = "Escaleno"
+        e_triangulo = f"Ele é um triângulo. Escaleno"
 else:
     e_triangulo = f"Ele não é um triangulo"
-print(f"{e_triangulo}{tipo}")
+print(f"{e_triangulo}.")

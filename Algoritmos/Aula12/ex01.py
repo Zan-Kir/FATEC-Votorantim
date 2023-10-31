53daf1dd6b45b10a2b1868866e93f6215568bcfb
Murilo Santos <murilo4santos@hotmail.com>
2023-10-30 22:20:31 -0300

    Algoritmos-30-10

---
+++ Algoritmos/Aula12/ex01.py
@@ -0,0 +1,14 @@
+dicIdade = {}
+maiorIdade = 0
+
+while len(dicIdade) < 5:
+    sobrenome = input("Digite o sobrenome: ")
+    idade = int(input("Digite a idade: "))
+    dicIdade[sobrenome] = idade
+
+for sobrenome, idade in dicIdade.items():
+    if idade > maiorIdade:
+        maiorIdade = idade
+        maisVelho = sobrenome
+
+print(f"A mais velho é o {maisVelho} com {maiorIdade} anos.")

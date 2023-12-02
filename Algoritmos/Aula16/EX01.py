c126ce76f3f8d72ae0a2d53b2452332dad75fa51
Zan-Kir <murilo4santos@hotmail.com>
2023-12-01 22:52:17 -0300

    Algoritmos-Aula16

--- Algoritmos/Aula16/EX01.py
+++ Algoritmos/Aula16/EX01.py
@@ -1,7 +1,9 @@
 
 Y = 22
-totalCabecas = Y * 2 + 5
-totalPes = Y * 3 + 7
+totalCabecas = int(input('Digite o total de cabeças: '))
+# Y * 2 + 5 = 49
+totalPes = int(input('Digite o total de pés: '))
+# Y * 3 + 7 = 73
 
 def peCabeca(cabecas, pes):
     for patos in range(0, cabecas + 1):
@@ -9,12 +11,12 @@ def peCabeca(cabecas, pes):
         if (2 * patos) + (4 * coelhos) == pes:
             return patos, coelhos
     else:
-        return False
+        return False, False
 
 #Tenho 49 cabeças e 73 pés segundo sua fórmula, sendo impossivel uma resolução.\
 patos, coelhos = peCabeca(totalCabecas, totalPes)
 
-if peCabeca(totalCabecas, totalPes) == False:
+if patos == False:
     print(f'Nâo existe solução possivel')
 else:
     print(f'total de patos é {patos}, total de coelhos é {coelhos}')
